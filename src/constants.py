f57117804daab4c138db91ee38a9e45a2a8e5438
AlexandreFleutelot <fleutelot.alexandre@gmail.Com>
2024-07-29 00:07:54 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -19,3 +19,7 @@ class Color(Enum):
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    CYAN = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+    ORANGE = (255, 165, 0)
